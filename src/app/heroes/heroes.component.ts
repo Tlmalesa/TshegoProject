de4d16282d5b5779545dd2310f15fea55b71cfaf
Codec-R312 <silasmaodi@gmail.com>
2021-06-09 15:44:40 +0200

    my first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  
+  selectedHero?: Hero;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
+}
